dfdbac2d37f9f2684faa0de1e3184d15d6617b98
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-01-15 22:54:44 -0300

    Category and Tag models

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from blog.models import Tag, Category
 # Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = 'name', 'slug',
+    list_display_links = 'name',
+    search_fields = 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {'slug': ('name',),}
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name', 'slug',
+    list_display_links = 'name',
+    search_fields = 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {'slug': ('name',),}
