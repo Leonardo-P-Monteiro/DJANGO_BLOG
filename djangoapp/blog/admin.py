34055ebf7eee22a6373637fff8604fe5ec1dd256
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-01-17 15:15:49 -0300

    Model Post Created and Registered

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from blog.models import Tag, Category
+from django.forms import ModelForm
+from django.http import HttpRequest
+from blog.models import Tag, Category, Page, Post
+from django.contrib.auth.models import User
+
 # Register your models here.
 
 @admin.register(Tag)
@@ -19,3 +23,35 @@ class CategoryAdmin(admin.ModelAdmin):
     list_per_page = 10
     ordering = '-id',
     prepopulated_fields = {'slug': ('name',),}
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'title', 'slug',
+    list_display_links = 'title',
+    search_fields = 'title', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published', 'created_by',
+    list_display_links = 'title',
+    search_fields = 'id', 'title', 'slug', 'content',
+    list_per_page = 50
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    prepopulated_fields = {'slug': ('title',)}
+    autocomplete_fields = 'tags', 'category',
+
+    def save_model(self, request: HttpRequest, obj: Post, form: ModelForm,
+                   change: bool) -> None:
+        
+        if change:
+            obj.updated_by = request.user #type: ignore
+        else:
+            obj.created_by = request.user #type: ignore
+
+        return super().save_model(request, obj, form, change)
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.rands import slugify_new
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,12 +14,12 @@ class Tag(models.Model):
                             max_length=255)
     
     def save(self, *args, **kwargs):
-        
         if not self.slug:
             self.slug = slugify_new(self.name, k=4)
-
         return super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     class Meta:
@@ -30,8 +31,68 @@ class Category(models.Model):
                             max_length=255)
     
     def save(self, *args, **kwargs):
-        
         if not self.slug:
             self.slug = slugify_new(self.name, k=4)
-        
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Page(models.Model):
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+    
+    title = models.CharField(max_length=200,)
+    slug = models.SlugField(max_length=255, unique=True, default="", null=False,
+                            blank=True,)
+    is_published = models.BooleanField(default=False, help_text="This field \
+                                       need to be marked for the page will be \
+                                       shown." )
+    content = models.TextField(default=None, null=True,)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, k=4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+    
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(max_length=255, unique=True, default='', null=True,
+                            blank=True)
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(default=False, help_text='This field need \
+                                     to be marked for the page will be shown')
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='',)
+    cover_in_post_content = models.BooleanField(default=True, help_text='If \
+                                                this field will marked, \
+                                                it will show the cover into the \
+                                                post.',)
+    created_at = models.DateTimeField(auto_now_add=True,)
+    created_by = models.ForeignKey(User, blank=True, null=True, 
+                                 on_delete=models.SET_NULL, 
+                                 related_name='post_created_by',)
+    updated_at = models.DateTimeField(auto_now=True,)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                   blank=True, related_name='post_updated_by')
+    category = models.ForeignKey(Category, models.SET_NULL, null=True,
+                                 blank=True, default=None,)
+    tags = models.ManyToManyField(Tag, blank=True, default='',)
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            self.slug = slugify_new(self.title, k=4)
+
+        return super().save(*args, *kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
